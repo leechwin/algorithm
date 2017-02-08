3621250294c1e6bcee9265dc796f9f36e663430b
ChangHyun <leechwin.lee@samsung.com>
2017-02-08 20:56:43 +0900

    implement for n-queen

---
+++ jungol_1889_N_Queen/README.md
@@ -0,0 +1,5 @@
+Reference
+
+창의적 알고리즘 (중급) : https://www.digitalculture.or.kr/upload/algorithm_md.pdf
+
+http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1162&sca=3030
--- jungol_1889_N_Queen/src/jungol_1889_N_Queen/Main.java
+++ jungol_1889_N_Queen/src/jungol_1889_N_Queen/Main.java
@@ -8,7 +8,10 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static int N;
-    public static int MAP[][];
+    public static int COL[];
+    public static int INC[]; // x + y
+    public static int DEC[]; // x - y
+    public static int CNT = 0;
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
@@ -18,11 +21,27 @@ public class Main {
         BufferedReader br = new BufferedReader(isr);
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        MAP = new int[N][N];
+        COL = new int[N];
+        INC = new int[N + N];
+        DEC = new int[N + N];
 
+        dfs(0); // start
+        System.out.println(CNT);
     }
 
-    public static void dfs(int x, int y) {
+    public static void dfs(int col) {
+        if (col >= N) {
+            CNT++;
+            return;
+        }
 
+        for (int row = 0; row < N; row++) {
+            if (COL[row] == 0 && INC[col + row] == 0 && DEC[(col - row) + N] == 0) {
+                COL[row] = INC[col + row] = DEC[(col - row) + N] = 1;
+                dfs(col + 1);
+                COL[row] = INC[col + row] = DEC[(col - row) + N] = 0;
+            }
+        }
     }
+
 }
