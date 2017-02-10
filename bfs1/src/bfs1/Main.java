526ae606cadb8e33e790fef380250ca2eddd77fd
ChangHyun <leechwin.lee@samsung.com>
2017-02-10 15:47:27 +0900

    add bfs problem

---
+++ bfs1/src/bfs1/Main.java
@@ -0,0 +1,21 @@
+package bfs1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = new FileInputStream("input.txt");
+        System.setIn(fis);
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println("");
+    }
+
+}
