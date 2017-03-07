a88d566408875daf655cb2c86636206fdf977aca
Changhyun lee <leechwin1@gmail.com>
2017-03-08 01:26:27 +0900

    add code piece

--- algospot_fence/src/algospot_fence/Main.java
+++ algospot_fence/src/algospot_fence/Main.java
@@ -3,6 +3,7 @@ package algospot_fence;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,6 +11,7 @@ public class Main {
     public static int C;
     public static int N;
     public static int HEIGHT[];
+    public static int VALUE[];
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
@@ -23,6 +25,7 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             HEIGHT = new int[N];
+            VALUE = new int[N];
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 HEIGHT[j] = Integer.parseInt(st.nextToken());
@@ -34,6 +37,25 @@ public class Main {
     }
 
     private static void solve() {
+        // 1 5 6 100 > 6
+        //
+        // 1 5 10 100 100
+        int topInex = 0;
+        int maxValue = HEIGHT[0];
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(HEIGHT[0]);
+
+        for (int i = 1; i < HEIGHT.length; i++) {
+            int preHeight = stack.peek();
+            int curHeight = HEIGHT[i];
+            if (preHeight > curHeight) {
+                topInex = i;
+                VALUE[i] = Math.max(maxValue, HEIGHT[i] * topInex + 1);
+            } else {
+
+            }
+        }
     }
 
 }
