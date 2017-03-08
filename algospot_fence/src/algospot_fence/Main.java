00161095686638ee416044b069dd16bd64014ba9
ChangHyun <leechwin.lee@samsung.com>
2017-03-08 19:02:19 +0900

    implement algospot fence

--- algospot_fence/src/algospot_fence/Main.java
+++ algospot_fence/src/algospot_fence/Main.java
@@ -10,8 +10,7 @@ public class Main {
 
     public static int C;
     public static int N;
-    public static int HEIGHT[];
-    public static int VALUE[];
+    public static int FENCE[];
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
@@ -24,38 +23,43 @@ public class Main {
         for (int i = 0; i < C; i++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            HEIGHT = new int[N];
-            VALUE = new int[N];
+            FENCE = new int[N + 1];
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                HEIGHT[j] = Integer.parseInt(st.nextToken());
+                FENCE[j] = Integer.parseInt(st.nextToken());
             }
+            FENCE[N] = 0; // end
 
-            solve();
+            System.out.println(solve());
         }
 
     }
 
-    private static void solve() {
-        // 1 5 6 100 > 6
-        //
-        // 1 5 10 100 100
-        int topInex = 0;
-        int maxValue = HEIGHT[0];
+    private static long solve() {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(0);
 
-        Stack<Integer> stack = new Stack<>();
-        stack.push(HEIGHT[0]);
+        long result = 0;
 
-        for (int i = 1; i < HEIGHT.length; i++) {
-            int preHeight = stack.peek();
-            int curHeight = HEIGHT[i];
-            if (preHeight > curHeight) {
-                topInex = i;
-                VALUE[i] = Math.max(maxValue, HEIGHT[i] * topInex + 1);
+        for (int i = 1; i <= N; i++) {
+            if (FENCE[stack.peek()] < FENCE[i]) {
+                stack.push(i);
             } else {
-
+                while (!stack.isEmpty() && FENCE[stack.peek()] > FENCE[i]) {
+                    // size = height * (right - left)
+                    // size = FENCE[top] * (i - prev stack index - 1)
+                    int top = stack.pop();
+                    if (stack.isEmpty()) {
+                        result = Math.max(result, FENCE[top] * i);
+                    } else {
+                        result = Math.max(result, FENCE[top] * (i - stack.peek() - 1));
+                    }
+                }
+                stack.push(i);
             }
         }
+
+        return result;
     }
 
 }
