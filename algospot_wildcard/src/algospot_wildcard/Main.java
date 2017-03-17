46814ae0fea6214bfffc01ad4037c605b9d7be21
ChangHyun <leechwin.lee@samsung.com>
2017-03-17 16:44:42 +0900

    add temp code

--- algospot_wildcard/src/algospot_wildcard/Main.java
+++ algospot_wildcard/src/algospot_wildcard/Main.java
@@ -7,6 +7,11 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    public static int C;
+    public static String W;
+    public static int N;
+    public static String[] WORD;
+
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
         System.setIn(fis);
@@ -14,7 +19,25 @@ public class Main {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // TODO
+        C = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < C; i++) {
+            st = new StringTokenizer(br.readLine());
+            W = st.nextToken();
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            WORD = new String[N];
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
+                WORD[j] = st.nextToken();
+            }
+
+            solve();
+        }
+    }
+
+    private static void solve() {
+        // TODO Auto-generated method stub
+
     }
 
 }
