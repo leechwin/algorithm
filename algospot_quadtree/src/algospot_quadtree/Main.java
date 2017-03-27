00340fe21f1e44ddb33e5441d65fefdb0d465db6
ChangHyun <leechwin.lee@samsung.com>
2017-03-27 19:54:44 +0900

    Implement for algospot quadtree problem

--- algospot_quadtree/src/algospot_quadtree/Main.java
+++ algospot_quadtree/src/algospot_quadtree/Main.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// O(n)
+// 144ms
 public class Main {
 
     public static int C;
+    public static int INDEX;
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
@@ -21,12 +24,27 @@ public class Main {
         for (int i = 0; i < C; i++) {
             st = new StringTokenizer(br.readLine());
             String str = st.nextToken();
-            solve(str);
+            INDEX = -1;
+            System.out.println(solve(str));
         }
     }
 
-    private static void solve(String str) {
-        // TODO
+    // not quad(!x) = return char
+    // if quad(x) = 1 -> 3, 2 -> 4
+    // 1, 2, 3, 4 -> 3, 4, 1, 2
+    private static String solve(String str) {
+        INDEX++;
+        char c = str.charAt(INDEX);
+        if (c == 'w' || c == 'b') {
+            return String.valueOf(c);
+        } else if (c == 'x') {
+            String str1 = solve(str);
+            String str2 = solve(str);
+            String str3 = solve(str);
+            String str4 = solve(str);
+            return "x" + str3 + str4 + str1 + str2;
+        }
+        return "";
     }
 
 }
