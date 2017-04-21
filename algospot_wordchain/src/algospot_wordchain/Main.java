fe7a495c3edaf80e639611cf8773384dbc3ef479
Changhyun lee <leechwin1@gmail.com>
2017-04-22 02:18:57 +0900

    Implement for algospot wordchain

--- algospot_wordchain/src/algospot_wordchain/Main.java
+++ algospot_wordchain/src/algospot_wordchain/Main.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,7 +16,9 @@ public class Main {
     public static int MAP[][] = new int[26][26];
     public static int IN_DEGREE[] = new int[26];
     public static int OUT_DEGREE[] = new int[26];
-    public static ArrayList<String> RESULT = new ArrayList<>();
+
+    public static LinkedList<String>[][] WORDMAP;
+    public static ArrayList<Integer> RESULT = new ArrayList<Integer>();
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
@@ -38,7 +41,7 @@ public class Main {
             }
             Arrays.fill(IN_DEGREE, 0);
             Arrays.fill(OUT_DEGREE, 0);
-
+            WORDMAP = new LinkedList[26][26];
             RESULT.clear();
 
             solve();
@@ -50,11 +53,13 @@ public class Main {
             makeGraph(i);
         }
 
-        if (RESULT.isEmpty() || RESULT.size() != N) {
+        getEulerCircuitOrTrail();
+        if (RESULT.size() - 1 != N) {
             System.out.println("IMPOSSIBLE");
         } else {
-            for (int i = RESULT.size() - 1; i >= 0; i--) {
-                System.out.printf(RESULT.get(i) + " ");
+            for (int i = RESULT.size() - 1; i >= 1; i--) {
+                String word = WORDMAP[RESULT.get(i)][RESULT.get(i - 1)].removeLast();
+                System.out.print(word + " ");
             }
             System.out.println();
         }
@@ -65,8 +70,39 @@ public class Main {
         int end = WORD[i].charAt(WORD[i].length() - 1) - 'a';
 
         MAP[start][end]++;
-        INDE
+        OUT_DEGREE[start]++;
+        IN_DEGREE[end]++;
+        if (WORDMAP[start][end] == null) {
+            WORDMAP[start][end] = new LinkedList<String>();
+        }
+        WORDMAP[start][end].add(WORD[i]);
+    }
 
+    private static void getEulerCircuit(int start) {
+        for (int end = 0; end < 26; end++) {
+            while (MAP[start][end] > 0) {
+                MAP[start][end]--;
+                getEulerCircuit(end);
+            }
+        }
+        RESULT.add(start);
+    }
+
+    private static void getEulerCircuitOrTrail() {
+        for (int i = 0; i < 26; i++) {
+            // find start node
+            if (IN_DEGREE[i] + 1 == OUT_DEGREE[i]) {
+                getEulerCircuit(i);
+                return;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (IN_DEGREE[i] > 0) {
+                getEulerCircuit(i);
+                return;
+            }
+        }
     }
 
 }
