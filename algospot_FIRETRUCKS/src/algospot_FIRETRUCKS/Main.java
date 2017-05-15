4e83820e729e5b1d67cf84686a078d2e89992e33
ChangHyun <leechwin.lee@samsung.com>
2017-05-15 22:22:59 +0900

    SUCCESS

--- algospot_FIRETRUCKS/src/algospot_FIRETRUCKS/Main.java
+++ algospot_FIRETRUCKS/src/algospot_FIRETRUCKS/Main.java
@@ -3,9 +3,9 @@ package algospot_FIRETRUCKS;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -18,8 +18,9 @@ public class Main {
 
     public static LinkedList<Vertex> ADJ[];
     public static int DIST[];
-    public static ArrayList<Integer> N;
-    public static ArrayList<Integer> M;
+
+    public static int[] N;
+    public static int[] M;
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
@@ -36,7 +37,7 @@ public class Main {
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
 
-            DIST = new int[V];
+            DIST = new int[V + 1];
             Arrays.fill(DIST, Integer.MAX_VALUE);
 
             ADJ = new LinkedList[V + 1];
@@ -57,27 +58,56 @@ public class Main {
                 ADJ[end].add(new Vertex(start, cost));
             }
 
-            N = new ArrayList<Integer>();
+            N = new int[n];
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                N.add(Integer.parseInt(st.nextToken()));
+                N[j] = Integer.parseInt(st.nextToken());
             }
 
-            M = new ArrayList<Integer>();
+            M = new int[m];
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                M.add(Integer.parseInt(st.nextToken()));
+                M[j] = Integer.parseInt(st.nextToken());
             }
 
-            solve();
-            System.out.println("end");
+            dijkstra();
+            int result = 0;
+            for (int j = 0; j < n; j++) {
+                result += DIST[N[j]];
+            }
+            System.out.println(result);
         }
 
     }
 
-    private static void solve() {
-        // TODO Auto-generated method stub
+    private static void dijkstra() {
+        PriorityQueue<Vertex> pq = new PriorityQueue<Vertex>();
+        for (int i = 0; i < m; i++) {
+            DIST[M[i]] = 0;
+            pq.offer(new Vertex(M[i], 0));
+        }
 
+        while (!pq.isEmpty()) {
+            Vertex vertex = pq.poll();
+            int here = vertex.here;
+            int cost = vertex.cost;
+            if (DIST[here] < cost) {
+                continue;
+            }
+
+            if (ADJ[here] != null) {
+                for (int i = 0; i < ADJ[here].size(); i++) {
+                    Vertex thereVertex = ADJ[here].get(i);
+                    int there = thereVertex.here;
+                    int thereCost = cost + thereVertex.cost;
+                    if (DIST[there] > thereCost) {
+                        DIST[there] = thereCost;
+                        pq.offer(new Vertex(there, thereCost));
+                    }
+
+                }
+            }
+        }
     }
 
     static class Vertex implements Comparable<Vertex> {
