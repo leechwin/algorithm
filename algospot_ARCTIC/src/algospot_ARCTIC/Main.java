4242e7b08f65744de077fd59e72c65d742d23d12
ChangHyun <leechwin.lee@samsung.com>
2017-07-14 13:49:29 +0900

    add problems

---
+++ algospot_ARCTIC/src/algospot_ARCTIC/Main.java
@@ -0,0 +1,31 @@
+package algospot_ARCTIC;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int C;
+    public static int N;
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = new FileInputStream("input.txt");
+        System.setIn(fis);
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        C = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < C; i++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+
+            }
+
+        }
+    }
+
+}
