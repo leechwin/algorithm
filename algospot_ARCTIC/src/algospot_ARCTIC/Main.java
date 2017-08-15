56bd20f02da5c3ef11fe103bd0d6f81bbe6d293c
Changhyun lee <leechwin1@gmail.com>
2017-08-15 22:48:26 +0900

    SUCCESS

--- algospot_ARCTIC/src/algospot_ARCTIC/Main.java
+++ algospot_ARCTIC/src/algospot_ARCTIC/Main.java
@@ -3,7 +3,8 @@ package algospot_ARCTIC;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,7 +14,11 @@ public class Main {
 
     public static double X[];
     public static double Y[];
-    public static ArrayList<Node> node = new ArrayList<Node>();
+    public static double DIST[][];
+    public static double MAX_DIST;
+    public static int parent[];
+    public static int rank[];
+    public static PriorityQueue<Node> pqueue = new PriorityQueue<>();
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("input.txt");
@@ -29,7 +34,11 @@ public class Main {
 
             X = new double[N];
             Y = new double[N];
-            node.clear();
+            parent = new int[N];
+            Arrays.fill(parent, -1);
+            rank = new int[N];
+            Arrays.fill(rank, -1);
+            DIST = new double[N][N];
 
             for (int j = 0; j < N; j++) {
                 st = new StringTokenizer(br.readLine());
@@ -37,27 +46,95 @@ public class Main {
                 Y[j] = Double.parseDouble(st.nextToken());
             }
 
-            solve();
+            calcDist();
+            mst();
+            System.out.printf("%.2f\n", MAX_DIST);
         }
     }
 
-    private static void solve() {
+    private static void mst() {
+        pqueue.clear();
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                // TODO:
+            pqueue.offer(new Node(0, i, DIST[0][i]));
+        }
+        MAX_DIST = 0;
+
+        int cnt = 0;
+        while (!pqueue.isEmpty()) {
+            if (cnt == N - 1) {
+                break;
+            }
+            Node node = pqueue.poll();
+            if (find(node.u) != find(node.v)) {
+                merge(node.u, node.v);
+                if (MAX_DIST < node.dist) {
+                    MAX_DIST = node.dist;
+                }
+                for (int i = 0; i < N; i++) {
+                    pqueue.offer(new Node(node.v, i, DIST[node.v][i]));
+                }
+                cnt++;
             }
         }
-        System.out.println("end");
     }
 
-    static class Node {
-        public double x;
-        public double y;
+    private static void merge(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v) {
+            return;
+        }
+        // 부모가될 v 가 더 크게 조정
+        if (rank[u] > rank[v]) {
+            int tmp = u;
+            u = v;
+            v = tmp;
+        }
+        if (rank[u] == rank[v]) {
+            rank[v]++;
+        }
+
+        parent[u] = v;
+    }
+
+    private static int find(int u) {
+        if (parent[u] == -1) {
+            return u;
+        }
+        parent[u] = find(parent[u]);
+        return parent[u];
+    }
+
+    private static void calcDist() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                DIST[i][j] = getRadius(X[i], X[j], Y[i], Y[j]);
+            }
+        }
+    }
+
+    private static double getRadius(double x1, double x2, double y1, double y2) {
+        return Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
+    }
+
+    static class Node implements Comparable<Node> {
+        public int u;
+        public int v;
         public double dist;
 
-        public Node(double x, double y) {
-            this.x = x;
-            this.y = y;
+        public Node(int u, int v, double dist) {
+            this.u = u;
+            this.v = v;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            if (this.dist > other.dist) {
+                return 1;
+            } else {
+                return -1;
+            }
         }
     }
 }
