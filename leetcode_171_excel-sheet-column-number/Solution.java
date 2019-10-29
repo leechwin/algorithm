7fe27dbc1951cd969b2b8faf879732b4f93906f8
ChangHyun Lee <leechwin.lee@samsung.com>
2019-10-29 19:59:10 +0900

    Update Solution.java

--- leetcode_171_excel-sheet-column-number/Solution.java
+++ leetcode_171_excel-sheet-column-number/Solution.java
@@ -1,24 +1,14 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int length = nums.length;
-        int i = 0;
-        int j = 0; // new array index
-        for (i = 0; i < length; i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
-            }
-        }
+    public int titleToNumber(String s) {
+        char[] ss = s.toCharArray();
 
-        if (j < length) {
-            while (j < length) {
-                // assign 0
-                nums[j++] = 0;
-            }
+        int sum = 0;
+        int pt = 0;
+        for (int i = ss.length - 1; i >= 0; i--) {
+            int charter = ((int) ss[i] - 64);
+            sum += Math.pow(26, pt) * charter;
+            pt++;
         }
-
-        for (int k = 0; k < length; k++) {
-            System.out.print(nums[k] + " ");
-        }
+        return sum;
     }
 }
