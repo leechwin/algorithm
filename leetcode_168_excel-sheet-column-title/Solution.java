9f206aca879071d34c24bb7e37ce621618c63a81
ChangHyun Lee <leechwin.lee@samsung.com>
2019-10-29 20:36:20 +0900

    leetcode: 168. Excel Sheet Column Title
    Signed-off-by: ChangHyun Lee <leechwin.lee@samsung.com>

---
+++ leetcode_168_excel-sheet-column-title/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String convertToTitle(int n) {
+         if (n <= 0) {
+            return "";
+        }
+
+        StringBuffer sb = new StringBuffer();
+        while (n > 0) {
+            n--;
+            int mod = n % 26;
+            char title = (char) (mod + 'A');
+            n = n / 26;
+            sb.append(title);
+        }
+
+        return sb.reverse().toString();
+    }
+}
