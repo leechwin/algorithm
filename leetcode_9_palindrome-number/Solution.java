98fb09f604202bc1964e4ed2b51c0948c9f85b9d
Changhyun lee <leechwin1@gmail.com>
2019-11-12 22:47:38 +0900

    leetcode 9: Palindrome Number

---
+++ leetcode_9_palindrome-number/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        String num = String.valueOf(x);
+        int start = 0;
+        int end = num.length() - 1;
+
+        while (start < end) {
+            if (num.charAt(start) != num.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
