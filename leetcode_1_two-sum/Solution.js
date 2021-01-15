05aff593bac66f35cef2a7601bd413749e703b19
Changhyun lee <leechwin1@gmail.com>
2021-01-16 00:01:00 +0900

    Add js solution

---
+++ leetcode_1_two-sum/Solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        // key: number, value: index
+        map[nums[i]] = i;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        const result = target - nums[i];
+
+        if (result in map) {
+            if (map[result] !== i) {
+                return [map[result], i];
+            }
+        }
+    }
+
+    return null;
+};
