3ad93a4f97d77d42257b9c85d5bbb6f42fad5bc0
ChangHyun Lee <leechwin1@gmail.com>
2021-01-31 01:16:32 +0900

    Update solution.js

--- programmers_12948_핸드폰번호가리기/solution.js
+++ programmers_12948_핸드폰번호가리기/solution.js
@@ -2,7 +2,7 @@
  * https://programmers.co.kr/learn/courses/30/lessons/12948
  */
 function solution(phone_number) {
-    var result = "*".repeat(phone_number.length - 4)
-        + phone_number.slice(-4);
+    // var result = "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+    var result = phone_number.slice(-4).padStart(phone_number.length, "*");
     return result;
 }
